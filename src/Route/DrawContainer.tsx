d94349f5821c3c610edf5e8c6c6bbc5e5c404dcc
jinseok <yeojinseuk@imcloud.co.kr>
2022-08-31 00:20:05 +0900

    version2

--- src/Route/DrawContainer.tsx
+++ src/Route/DrawContainer.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { Wrapper } from '../Layout/DefaultLayout'
 import * as rxjs from 'rxjs'
-import { click } from '@testing-library/user-event/dist/click'
 
 const CanvasContainer = styled.div`
   height: 100vh;
@@ -12,76 +11,52 @@ const CanvasContainer = styled.div`
   justify-content: center;
 `
 const Canvas = styled.canvas`
-  width: 100%;
-  height: 100%;
+  width: 80%;
+  height: 80%;
   background-color: green;
 `
 
 export default function DrawContainer() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const painting = new rxjs.BehaviorSubject<CanvasRenderingContext2D>(null)
+  const painting = new rxjs.BehaviorSubject(false)
   const [context, setContext] = useState(null)
 
   useEffect(() => {
     if (canvasRef.current) {
       const context = canvasRef.current.getContext('2d')
       context.lineWidth = 3
-      context.strokeStyle = 'white'
+      context.strokeStyle = 'blue'
       context.font = '16px sans-serif'
-      painting.next(context)
-      DrawController(canvasRef.current)
+      setContext(context)
     }
   }, [canvasRef])
 
-  const DrawController = (canvasRef: HTMLCanvasElement) => {
-    rxjs
-      .fromEvent(canvasRef, 'click')
-      .pipe(
-        rxjs.map((e: any) => {
-          return {
-            x: e.x,
-            y: e.y,
-          }
-        }),
-        rxjs.startWith({ x1: 0, y1: 0, x2: null, y2: null }),
-        rxjs.scan((acc: any, cur: any) => {
-          return { x1: acc.x2, y1: acc.y2, x2: cur.x, y2: cur.y }
-        }),
-        rxjs.switchMap(xy =>
-          rxjs.interval(10).pipe(
-            rxjs.startWith({ x1: xy.x1, y1: xy.y1, x2: xy.x1, y2: xy.y1 }),
-            rxjs.scan((acc: any, cur: any) => {
-              return {
-                x1: acc.x1,
-                y1: acc.y1,
-                x2: acc.x2 + (xy.x2 - xy.x1) / 100,
-                y2: acc.y2 + (xy.y2 - xy.y1) / 100,
-              }
-            }),
-            rxjs.take(100)
-          )
-        )
-      )
-      .subscribe(DrawLine)
-  }
-  function DrawLine(xy: any) {
+  const Draw = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     painting
       .pipe(
-        rxjs.map(context => {
-          context.beginPath()
-          context.moveTo(xy.x1, xy.y1)
-          context.lineTo(xy.x2, xy.y2)
-          context.closePath()
-          context.stroke()
-        })
+        rxjs.map(x => {
+          if (!x) {
+            context.beginPath()
+            context.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+          } else {
+            context.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+            context.stroke()
+          }
+        }),
+        rxjs.delay(1000)
       )
       .subscribe()
   }
-
   return (
     <Wrapper>
       <CanvasContainer>
-        <Canvas ref={canvasRef} />
+        <Canvas
+          ref={canvasRef}
+          onMouseDown={() => painting.next(true)}
+          onMouseUp={() => painting.next(false)}
+          onMouseLeave={() => painting.next(false)}
+          onMouseMove={e => Draw(e)}
+        />
       </CanvasContainer>
     </Wrapper>
   )
